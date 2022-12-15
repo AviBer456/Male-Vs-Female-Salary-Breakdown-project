6f1f9a3520521e2397c124ff277b4f18266414ec
AviBer456 <120674173+AviBer456@users.noreply.github.com>
2022-12-15 12:15:39 -0600

    Update Male VS Female Salary Breackdown project.sql

--- Male VS Female Salary Breackdown project.sql
+++ Male VS Female Salary Breackdown project.sql
@@ -1,5 +1,7 @@
-/* In this project I will analyze the data from the database employees_mod.
-The tables that are created will feature on tableau here: https://public.tableau.com/app/profile/avi.berckovitch/viz/MaleVSFemaleSalariesBreakdown/Dashboard1 */
+/* In this project I will analyze the data from the database: 
+https://raw.githubusercontent.com/UsmanNiazi/365datascience/master/10.%20SQL%20%2B%20Tablue/Excercise%20Files/employees_mod.sql .
+The tables that are created will feature on a tableau dashboard here: 
+https://public.tableau.com/app/profile/avi.berckovitch/viz/MaleVSFemaleSalariesBreakdown/Dashboard1 */
 
 
 USE employees_mod;
